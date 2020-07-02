59c78c55a8bf48ec92d6fffba39ede72767603bc
kaku-kuo <z22857744@gmail.com>
2020-07-02 18:39:10 +0800

    connect to clearDB

--- connection/db_connect.php
+++ connection/db_connect.php
@@ -1,4 +1,4 @@
 <?php
-$conn = mysqli_connect('us-cdbr-east-02.cleardb.com','bcc5d75ec65bec','04f058c6','heroku_b6c68810e24d9c4');
+$conn = mysqli_connect('us-cdbr-east-02.cleardb.com','bcc5d75ec65bec','04f058c6','Tables');
 ?>
 
