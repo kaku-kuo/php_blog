be529366897e6cc480f07b3e0735e7c54af08c40
kaku-kuo <z22857744@gmail.com>
2020-07-02 22:13:17 +0800

    d

--- connection/db_connect.php
+++ connection/db_connect.php
@@ -1,12 +1,12 @@
 <?php
 
-$url=parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-$dbhost = $url["host"];
-$dbuser = $url["user"];
-$dbpass = $url["pass"];
-$dbname = substr($url["path"],1);
 
-$conn = mysqli_connect($dbhost, $dbuser, $dbpass,$dbname);
+$dbhost = 'us-cdbr-east-02.cleardb.com';
+$user = 'bcc5d75ec65bec';
+$pass = '04f058c6';
+$dbname = 'heroku_b6c68810e24d9c4';
+
+$conn = mysqli_connect($dbhost, $user, $pass ,$dbname);
 ?>
 
