7174eb55aedb3f0aa11953281d9b65ae3da8b674
kaku-kuo <z22857744@gmail.com>
2020-07-03 15:42:28 +0800

    connect file changed

--- connection/db_connect.php
+++ connection/db_connect.php
@@ -8,6 +8,7 @@ $cleardb_username = $cleardb_url["user"];
 $cleardb_password = $cleardb_url["pass"];
 $cleardb_db       = substr($cleardb_url["path"],1);
 
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
 $active_group = 'default';
 $query_builder = TRUE;
